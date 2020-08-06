11564d21188a2b431dfc651c778b6d04e77d41e1
Ezeugo Anukem <jea2161@columbia.edu>
2020-08-05 21:30:44 -0700

    Finished lab 4

--- variants.ml
+++ variants.ml
@@ -12,6 +12,7 @@ type quad = I | II | III | IV
 type sign = Neg | Zero | Pos
 
 let sign (x:int) : sign = if x < 0 then Neg else if x = 0 then Zero else Pos
+let new_sign x = if x < 0 then `Neg else if x = 0 then `Zero else `Pos
 
 let quadrant : int*int -> quad option = fun (x,y) ->
   match sign x, sign y with
@@ -21,6 +22,15 @@ let quadrant : int*int -> quad option = fun (x,y) ->
     | Pos, Neg -> Some IV
     | Zero, _ -> None
     | _, Zero -> None
+
+let new_quadrant (x, y) =
+  match new_sign x, new_sign y with
+    | `Pos, `Pos -> Some `I
+    | `Neg, `Pos -> Some `II
+    | `Neg, `Neg -> Some `III
+    | `Pos, `Neg -> Some `IV
+    | `Zero, _ -> None
+    | _, `Zero -> None
 
 let quadrant_when (x, y) =
   match x, y with
@@ -48,4 +58,55 @@ let rec same_shape t1 t2 =
 let rec int_max int_list =
   match int_list with 
   | [] -> failwith "int_max failure"
+  | h::[] -> h
   | h::tl -> max h (int_max tl)
+
+let rec int_max_string int_list = 
+  match int_max int_list with
+    | s -> string_of_int s
+    | exception (Failure s) -> "empty"
+
+let rec lookup key tree = 
+  match tree with 
+  | Node ((k, v), left, right) when key = k -> Some v
+  | Node ((k, v), left, right) when key > k -> lookup key right
+  | Node ((k, v), left, right) when key < k -> lookup key left
+  | Node (_, left, right) -> failwith "Not a valid node"
+  | Leaf -> None
+
+let rec insert key value tree =
+  match tree with
+  | Node ((k, v), left, right) when key > k -> Node ((k, v), left, (insert key value right)) 
+  | Node ((k, v), left, right) when key < k -> Node ((k, v), (insert key value left), right)
+  | Node ((k, v), left, right) when key = k -> Node ((key, value), left, right) 
+  | Node (_, left, right) -> failwith "Not a valid node"
+  | Leaf -> Node ((key, value), Leaf, Leaf)
+
+let rec minimium tree = 
+  match tree with 
+  | Leaf -> failwith "empty tree"
+  | Node ((k, v), Leaf, Leaf) -> k
+  | Node ((k, v), Node ((a, b), c, d), Leaf) -> min k (minimium (Node ((a, b), c, d)))
+  | Node ((k, v),Leaf, Node ((a, b), c, d)) -> min k (minimium (Node ((a, b), c, d)))
+  | Node ((k, v), Node ((e, f), g, h), Node ((a, b), c, d)) -> min (minimium (Node ((e, f), g, h))) (min k (minimium (Node ((a, b), c, d))))
+
+
+let rec maximium tree = 
+  match tree with 
+  | Leaf -> failwith "empty tree"
+  | Node ((k, v), Leaf, Leaf) -> k
+  | Node ((k, v), Node ((a, b), c, d), Leaf) -> max k (maximium (Node ((a, b), c, d)))
+  | Node ((k, v),Leaf, Node ((a, b), c, d)) -> max k (maximium (Node ((a, b), c, d)))
+  | Node ((k, v), Node ((e, f), g, h), Node ((a, b), c, d)) -> max (maximium (Node ((e, f), g, h))) (max k (maximium (Node ((a, b), c, d))))
+
+
+let is_bst tree = 
+  let _max = maximium tree in
+  let _min = minimium tree in 
+  let rec is_valid_bst high low tree = 
+    match tree with
+    | Leaf -> true
+    | Node ((k, v), left, right) -> if k >= low && k <= high then is_valid_bst k _min left && is_valid_bst _max k right else false
+
+  in is_valid_bst _max _min tree
+
--- variants_test.ml
+++ variants_test.ml
@@ -2,14 +2,14 @@ open OUnit2
 open Variants
 
 let tree =
-  Node(4,
-    Node(2,
-      Node(1,Leaf,Leaf),
-      Node(3,Leaf,Leaf)
+  Node((4, "four"),
+    Node((2, "two"),
+      Node((1, "one"),Leaf,Leaf),
+      Node((3, "three"),Leaf,Leaf)
     ),
-    Node(5,
-      Node(6,Leaf,Leaf),
-      Node(7,Leaf,Leaf)
+    Node((5, "five"),
+      Node((6, "six"),Leaf,Leaf),
+      Node((7, "seven"),Leaf,Leaf)
     )
   )
 
@@ -26,12 +26,19 @@ let same_tree =
     )
   )
 
+let my_tree = Node ((4, "four"), 
+                Node ((2, "two"), Leaf, Leaf),
+                Node ((5, "five"), Leaf, Leaf))
+
 let leaf = Leaf
 
 let tests = "test suite for variants" >::: [
-  "check the tree against itself"     >:: (fun _ -> assert_equal true (same_shape tree tree));
-  "different values, same tree shape" >:: (fun _ -> assert_equal true (same_shape same_tree tree));
-  "different trees altogether"        >:: (fun _ -> assert_equal false (same_shape leaf tree));
+  "check the tree against itself"                        >:: (fun _ -> assert_equal true (same_shape tree tree));
+  "different values, same tree shape"                    >:: (fun _ -> assert_equal true (same_shape same_tree tree));
+  "different trees altogether"                           >:: (fun _ -> assert_equal false (same_shape leaf tree));
+  "is_bst succeeds on valid input"                       >:: (fun _ -> assert_equal true (is_bst my_tree));
+  "int_max_string throws exception on empty list"        >:: (fun _ -> assert_equal "empty" (int_max_string []));
+  "int_max_string throws exception on empty list"        >:: (fun _ -> assert_raises (Failure "int_max failure") (fun x -> int_max []));
 ]
 
 let _ = run_test_tt_main tests
