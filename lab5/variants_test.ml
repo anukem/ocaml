da26a996f920cabc700d29127ec5e710aa2e06b6
Ezeugo Anukem <jea2161@columbia.edu>
2021-06-06 08:40:49 -0700

    Added more tests

--- lab5/variants_test.ml
+++ lab5/variants_test.ml
@@ -26,9 +26,18 @@ let same_tree =
     )
   )
 
-let my_tree = Node ((4, "four"), 
+let my_tree = Node ((4, "four"),
                 Node ((2, "two"), Leaf, Leaf),
                 Node ((5, "five"), Leaf, Leaf))
+
+
+let bad_tree = Node ((4, "four"),
+                Node ((6, "two"), Leaf, Leaf),
+                Node ((298, "five"), Leaf, Leaf))
+
+let other_bad_tree = Node ((4, "four"),
+                Node ((1, "two"), Leaf, Leaf),
+                Node ((1, "five"), Leaf, Leaf))
 
 let leaf = Leaf
 
@@ -36,7 +45,11 @@ let tests = "test suite for variants" >::: [
   "check the tree against itself"                        >:: (fun _ -> assert_equal true (same_shape tree tree));
   "different values, same tree shape"                    >:: (fun _ -> assert_equal true (same_shape same_tree tree));
   "different trees altogether"                           >:: (fun _ -> assert_equal false (same_shape leaf tree));
+  "it calculates max correctly"                          >:: (fun _ -> assert_equal 7 (maximium tree));
+  "it calculates min correctly"                          >:: (fun _ -> assert_equal 1 (minimium tree));
   "is_bst succeeds on valid input"                       >:: (fun _ -> assert_equal true (is_bst my_tree));
+  "is_bst fails on invalid input"                        >:: (fun _ -> assert_equal false (is_bst bad_tree));
+  "is_bst fails on invalid input when right is greater"  >:: (fun _ -> assert_equal false (is_bst bad_tree));
   "int_max_string throws exception on empty list"        >:: (fun _ -> assert_equal "empty" (int_max_string []));
   "int_max_string throws exception on empty list"        >:: (fun _ -> assert_raises (Failure "int_max failure") (fun x -> int_max []));
 ]
